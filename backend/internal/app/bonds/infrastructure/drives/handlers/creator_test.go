15728590e0200ee4e12878e4447d7c6d85edb7d4
erik-sostenes <eriksostenessimon@gmail.com>
2024-06-11 00:50:16 -0600

    feat(bonds): implement error handling middleware for HTTP handlers
    - Implemented middleware to wrap HTTP handlers for error control.
    - Refactored acceptance tests to ensure proper error handling.
    - Added tests to verify and secure business logic.

--- backend/internal/app/bonds/infrastructure/drives/handlers/creator_test.go
+++ backend/internal/app/bonds/infrastructure/drives/handlers/creator_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -39,16 +40,7 @@ func Test_PostBondHandler(t *testing.T) {
 			expectedStatusCode: http.StatusCreated,
 		},
 		"Given an invalid non-existing bond a status code 422 is expected": {
-			request: httptest.NewRequest(http.MethodPost, "/api/v1/bonds/create", strings.NewReader(
-				`{
-					"id": "ba1dc545-90a0-4501-af99-8a5944ca38c4",
-					"name": "Global Secure Corporate Bond 2024",
-					"quantity_sale": 1.5,
-					"sales_price": 400.0000,
-					"creator_user_id":"580b87da-e389-4290-acbf-f6191467f401",
-					"current_owner_id":"580b87da-e389-4290-acbf-f6191467f401"
-				}`,
-			)),
+			request: httptest.NewRequest(http.MethodPost, "/api/v1/bonds/create", http.NoBody),
 			handlerFunc: func() (response.HttpHandlerFunc, error) {
 				memo := memory.NewBondMemory()
 				bondCtr := logic.NewBondCreator(&memo)
@@ -56,6 +48,101 @@ func Test_PostBondHandler(t *testing.T) {
 				return PostBondHandler(bondCtr), nil
 			},
 			expectedStatusCode: http.StatusUnprocessableEntity,
+		},
+		"Given the name of an invalid bond, the status code 400 is expected": {
+			request: httptest.NewRequest(http.MethodPost, "/api/v1/bonds/create", strings.NewReader(
+				`{
+					"id": "ba1dc545-90a0-4501-af99-8a5944ca38c4",
+					"name": "Ultra Secure Global Investment Corporate Bond 2045",
+					"quantity_sale": 1,
+					"sales_price": 400.0000,
+					"creator_user_id":"580b87da-e389-4290-acbf-f6191467f401",
+					"current_owner_id":"580b87da-e389-4290-acbf-f6191467f401"
+				}`,
+			)),
+
+			handlerFunc: func() (response.HttpHandlerFunc, error) {
+				memo := memory.NewBondMemory()
+				bondCtr := logic.NewBondCreator(&memo)
+
+				return PostBondHandler(bondCtr), nil
+			},
+			expectedStatusCode: http.StatusBadRequest,
+		},
+		"Given the quantity sale of an invalid bond, the status code 400 is expected": {
+			request: httptest.NewRequest(http.MethodPost, "/api/v1/bonds/create", strings.NewReader(
+				`{
+					"id": "ba1dc545-90a0-4501-af99-8a5944ca38c4",
+					"name": "Global Secure Corporate Bond 2024",
+					"quantity_sale": 10001,
+					"sales_price": 400.0000,
+					"creator_user_id":"580b87da-e389-4290-acbf-f6191467f401",
+					"current_owner_id":"580b87da-e389-4290-acbf-f6191467f401"
+				}`,
+			)),
+
+			handlerFunc: func() (response.HttpHandlerFunc, error) {
+				memo := memory.NewBondMemory()
+				bondCtr := logic.NewBondCreator(&memo)
+
+				return PostBondHandler(bondCtr), nil
+			},
+			expectedStatusCode: http.StatusBadRequest,
+		},
+		"Given the sales price of an invalid bond, the status code 400 is expected": {
+			request: httptest.NewRequest(http.MethodPost, "/api/v1/bonds/create", strings.NewReader(
+				`{
+					"id": "ba1dc545-90a0-4501-af99-8a5944ca38c4",
+					"name": "Global Secure Corporate Bond 2024",
+					"quantity_sale": 10000,
+					"sales_price": 100000001.0000,
+					"creator_user_id":"580b87da-e389-4290-acbf-f6191467f401",
+					"current_owner_id":"580b87da-e389-4290-acbf-f6191467f401"
+				}`,
+			)),
+
+			handlerFunc: func() (response.HttpHandlerFunc, error) {
+				memo := memory.NewBondMemory()
+				bondCtr := logic.NewBondCreator(&memo)
+
+				return PostBondHandler(bondCtr), nil
+			},
+			expectedStatusCode: http.StatusBadRequest,
+		},
+		"Given an existing valid bond, a 400 status code is expected": {
+			request: httptest.NewRequest(http.MethodPost, "/api/v1/bonds/create", strings.NewReader(
+				`{
+					"id": "ba1dc545-90a0-4501-af99-8a5944ca38c4",
+					"name": "Global Secure Corporate Bond 2024",
+					"quantity_sale": 1,
+					"sales_price": 400.0000,
+					"creator_user_id":"580b87da-e389-4290-acbf-f6191467f401",
+					"current_owner_id":"580b87da-e389-4290-acbf-f6191467f401"
+				}`,
+			)),
+			handlerFunc: func() (response.HttpHandlerFunc, error) {
+				bondRequest := BondRequest{
+					ID:             "ba1dc545-90a0-4501-af99-8a5944ca38c4",
+					Name:           "Global Secure Corporate Bond 2024",
+					QuantitySale:   1,
+					SalesPrice:     400.0000,
+					CreatorUserId:  "580b87da-e389-4290-acbf-f6191467f401",
+					CurrentOwnerId: "580b87da-e389-4290-acbf-f6191467f401",
+				}
+
+				bond, err := bondRequest.toBusiness()
+				if err != nil {
+					return nil, err
+				}
+
+				memo := memory.NewBondMemory()
+
+				// save in memory a new Bond
+				memo.Save(context.Background(), bond)
+
+				return PostBondHandler(logic.NewBondCreator(&memo)), nil
+			},
+			expectedStatusCode: http.StatusBadRequest,
 		},
 	}
 
--- backend/internal/app/bonds/infrastructure/drives/handlers/errors.go
+++ backend/internal/app/bonds/infrastructure/drives/handlers/errors.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/erik-sostenes/bonds-publisher-challenge/internal/app/bonds/business/domain"
 	"github.com/erik-sostenes/bonds-publisher-challenge/pkg/server/response"
 )
 
@@ -10,6 +12,44 @@ import (
 func BondErrorHandler(apiFunc response.HttpHandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := apiFunc(w, r); err != nil {
+			asBond := domain.BondError(0)
+
+			if errors.As(err, &asBond) {
+				message := response.Response{
+					Code:    asBond.Error(),
+					Message: err.Error(),
+				}
+
+				switch asBond {
+				case domain.InvalidBondID,
+					domain.InvalidBondName,
+					domain.InvalidBondQuantitySale,
+					domain.InvalidBondSalesPrice,
+					domain.DuplicateBond:
+					_ = response.JSON(w, http.StatusBadRequest, message)
+					return
+				default:
+					_ = response.JSON(w, http.StatusBadRequest, message)
+					return
+				}
+			}
+
+			asUser := domain.UserError(0)
+			if errors.As(err, &asUser) {
+				message := response.Response{
+					Code:    asBond.Error(),
+					Message: err.Error(),
+				}
+
+				switch asUser {
+				case domain.InvalidUserID:
+					_ = response.JSON(w, http.StatusBadRequest, message)
+					return
+				default:
+					_ = response.JSON(w, http.StatusBadRequest, message)
+					return
+				}
+			}
 			return
 		}
 	}
